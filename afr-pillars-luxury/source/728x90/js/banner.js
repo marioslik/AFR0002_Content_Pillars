d9fce8f628d59982c1aeac46e133498d879f2542
marioslik <mario@slik.com.au>
2017-05-05 13:52:51 +1000

    Adding 728x90 luxury

--- afr-pillars-luxury/source/728x90/js/banner.js
+++ afr-pillars-luxury/source/728x90/js/banner.js
@@ -19,16 +19,16 @@ function init() {
 
     // Init CSS
     // =========================
-    TweenMax.set('.feature', {x:100, y:200, display:'block'})
+    TweenMax.set('.feature', {x:250, y:200, scale:0.5, display:'block'})
     TweenMax.set('.logo-container', {y:26});
     TweenMax.set('.cta-arrow', {rotation:45, transformOrigin:'50% 50%'});
-    TweenMax.set('.panel-03', {x:-300});
-    TweenMax.set('.panel-04', {x:-300});
-    TweenMax.set('.panel-05', {x:-300});
+    TweenMax.set('.panel-03', {x:-728}); // Reset the panels
+    TweenMax.set('.panel-04', {x:-728}); // Reset the panels
+    TweenMax.set('.panel-05', {x:-728}); // Reset the panels
     TweenMax.set('.graph-line', {scaleX:0});
     TweenMax.set('#fr-logo-intro', {opacity:1});
-    TweenMax.set('.cloud1', {x:10, y:120});
-    TweenMax.set('.cloud2', {x:140, y:90});
+    TweenMax.set('.cloud1', {x:150, y:12});
+    TweenMax.set('.cloud2', {x:240, y:50});
     TweenMax.set('.cloud3', {x:200, y:0});
 
 
@@ -50,8 +50,8 @@ function init() {
 
 
     $('.frame-2 p').html('LIVING THE<br>HIGH LIFE');
-    $('.frame-3 p').html('A CLOSER<br>LOOK AT LIVING<br>IN LUXURY');
-    $('.frame-4 p').html('MIX BUSINESS<br>AND LEISURE<br>WITH THE AFR');
+    $('.frame-3 p').html('A CLOSER LOOK AT<br>LIVING IN LUXURY');
+    $('.frame-4 p').html('MIX BUSINESS AND<br>LEISURE WITH THE AFR');
     $('.frame-5 p').html('TRY THE AFR<br>FREE FOR 1 MONTH');
 
     $('.cta-copy').html('Find out more');
@@ -140,12 +140,12 @@ function start() {
 
 
     function featureAnimation() {
-        tlFeature.to('.feature', 5, {ease: Back.easeOut.config(1), scale:0.7, x:110, y:-40})
-                 .to('.cloud2', 6, {ease: Power1.easeInOut, x:"-=20", y:"-=20"}, "-=5")
-                 .to('.cloud3', 8, {ease: Power1.easeInOut, x:"-=20", y:"-=20"}, "-=6")
-                 .to('.cloud1', 4, {ease: Power1.easeInOut, x:"-=20", y:"-=20"}, "-=8")
+        tlFeature.to('.feature', 5, {ease: Back.easeOut.config(1), scale:0.5, x:110, y:-80})
+                 .to('.cloud2', 6, {ease: Power1.easeInOut, x:"+=40", y:"-=20"}, "-=5")
+                 .to('.cloud3', 8, {ease: Power1.easeInOut, x:"+=40", y:"-=40"}, "-=6")
+                 .to('.cloud1', 6, {ease: Power1.easeInOut, x:"+=90", y:"-=30"}, "-=8")
                 //  .to('.feature', 3, {ease: Power1.easeInOut, scale:0.6, x:120, y:-30}, "-=9.5")
-                 .to('.feature', 5, {ease: Back.easeOut.config(1), scale:0.45, x:120, y:-60}, '-=4.4')
+                 .to('.feature', 5, {ease: Back.easeOut.config(1), scale:0.45, x:120, y:-40}, '-=4')
         ;
         }
 
@@ -155,12 +155,12 @@ function start() {
 
 
     tl
-      .to('.panel-01', 1.6, {y:250, ease:Power1.easeInOut})
-      .to('.panel-02', 1.1, {y:195, ease:Power1.easeInOut, backgroundColor:"#ffffff", opacity:1, onComplete: () => {
-          TweenMax.set('.panel-01', {y:-250}) // Reset the panels
+      .to('.panel-01', 1.6, {x:500, ease:Power1.easeInOut})
+      .to('.panel-02', 1.1, {x:500, ease:Power1.easeInOut, backgroundColor:"#ffffff", opacity:1, onComplete: () => {
+          TweenMax.set('.panel-01', {x:-768}) // Reset the panels
 
       }}, '-=1.1')
-      .to('#fr-logo-intro', 1.1, {ease:Power1.easeInOut, top:"209px"}, "-=1.1")
+      .to('#fr-logo-intro', 1.1, {ease:Power1.easeInOut, left:"518px"}, "-=1.1")
       .to('#tag-line-intro', 1.1, {ease:Power1.easeInOut, opacity:0}, "-=1.1")
       .to('#logo-lockup-intro .fr-logo-path', 0.5, {ease:Power1.easeInOut, fill:"#1289ca"}, "-=0.85")
       .call(featureAnimation, [], this, "-=0.9")
@@ -175,18 +175,21 @@ function start() {
 
 
 
-      .to('.panel-05', 1.6, {x:0, ease:Power1.easeInOut}, 'frame04')
-      .to('.panel-04', 1.3, {x:0, ease:Power1.easeInOut}, 'frame04+=.3')
-      .to('.panel-03', 1.1, {x:0, ease:Power1.easeInOut}, 'frame04+=.5')
+      .to('.panel-05', 1.6, {x:-222, ease:Power1.easeInOut}, 'frame04')
+      .to('.panel-04', 1.3, {x:-222, ease:Power1.easeInOut}, 'frame04+=.3')
+      .to('.panel-03', 1.1, {x:-222, ease:Power1.easeInOut}, 'frame04+=.5')
+
       .staggerFrom('.frame-4 p .splitLines', 0.8, {y:30, alpha:0, ease:Power1.easeInOut}, 0.08, 'frame04+=1')
-      .staggerTo('.frame-4 p .splitLines', 0.8, {y:-30, alpha:0, ease:Power1.easeInOut, delay:1.5}, 0.08)
+      .staggerTo('.frame-4 p .splitLines', 0.8, {y:-30, alpha:0, ease:Power1.easeInOut, delay:2}, 0.08)
+
+
 
       .staggerFrom('.frame-5 p .splitLines', 0.8, {y:30, alpha:0, ease:Power1.easeInOut}, 0.08)
 
 
       .from('.button', 0.8, {alpha:0, y:20, ease:Power1.easeOut}, "-=0.2")
-      .to('.panel-02', 1, {y:170, ease:Power1.easeInOut}, "-=1.3")
-      .to('#fr-logo-intro', 1, {y:-21, ease:Power1.easeInOut}, "-=1.3")
+      .to('.panel-02', 1, {y:'+=0', ease:Power1.easeInOut}, "-=1.3")
+      .to('#fr-logo-intro', 1, {y:-10, ease:Power1.easeInOut}, "-=1.3")
       .to('#tag-line-intro', 1, {ease:Power1.easeInOut, opacity:1}, "-=1.3")
       .to('#tag-line-intro .fr-logo-path', 1, {ease:Power1.easeInOut, fill:"#1289ca"}, "-=1.3")
       .from('#tag-line', 1, {alpha:0, ease:Power1.easeOut}, 'endFrame+=1')
